88c46fa5867eaba2407a84075bd47f16ffd806d7
Singanimanasa <manasanaidu37@gmail.com>
2023-06-07 16:04:31 +0530

    first commit

---
+++ HolaApp/App.js
@@ -0,0 +1,88 @@
+// import React from "react"
+// import {View, Text} from "react-native"
+
+// const App = () => {
+//   return (
+//     <View style ={{width: 100,
+//         height: 100,
+//         backgroundColor: "red",}}> 
+//       </View>
+//   )
+// }
+
+// export default  App
+
+
+//  import React from "react"
+// import {View, Text} from "react-native"
+// const App = () => {
+//   return (
+//      <View style ={{width: 100,
+//         height: 50,
+//          backgroundColor: "red",}}> 
+//        </View>
+//    )import React from "react"
+
+//  }
+
+//  export default  App
+
+// import {View, Text} from "react-native"
+// const App = () => {
+//   return (
+//      <View style ={{
+  //       width: 100,
+//         height: 100,
+//         borderRadius: 100/2,
+//         backgroundColor: "red",}}> 
+//        </View>
+//    )
+//  }
+
+//  export default  App
+
+import {View, Text} from "react-native"
+const App = () => {
+  return (
+    <View style ={{width:"100%",alignItems:"center"}}>
+     <View style ={{width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: "red",
+        paddingLeft:50,
+        transform: [{scaleX:2}]}}> 
+       </View>
+       </View>
+   )
+ }
+
+ export default  App
+
+// import {View, Text} from "react-native"
+// const App = () => {
+//   return (
+//      <View style ={{width: 100,
+//         height: 100,
+//         borderRadius: 50,
+//         backgroundColor: "red",
+//         transform: [{scaleY:2}]}}> 
+//        </View>
+//    )
+//  }
+
+//  export default  App
+
+ //text
+//  import {View, Text} from "react-native"
+// const App = () => {
+//   return (
+//     <view> 
+//      <text style ={{frontWeight: 'bold'}}>
+//       I am bold
+//       <text style={{color:'red'}}>and red</text>
+//      </text>
+//      </view>
+//    )
+//  }
+
+//  export default  App
